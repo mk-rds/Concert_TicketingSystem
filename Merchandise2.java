995f0c4bac072119b0605d3ac7fae6cbf60e8822
LeeHanShen <77600251+LeeHanShen@users.noreply.github.com>
2021-03-04 16:26:01 +0800

    Update Merchandise2.java

--- Merchandise2.java
+++ Merchandise2.java
@@ -1,5 +1,8 @@
 package Assignment;
 
+import static assignment.StockRemaining.MerchStock_1;
+import static assignment.StockRemaining.MerchStock_2;
+import static assignment.StockRemaining.MerchStock_3;
 import java.util.Scanner;
 
 /**
@@ -13,9 +16,9 @@ public class Merchandise2 {
     public static double runningTotal;
     static boolean ordering = true;
     public static double quantity2;
-    static String c1 = String.format("\n1) BLACKPINK CONCERT" + "           2) TAYLOR SWIFT CONCERT" + "                      3) JAY CHOU CONCERT" + "          4) PURCHASE COMPLETE/EXIT");
-    static String c2 = String.format("\n   BLACKPINK SIGNED T-SHIRTS" + "      TAYLOR SWIFT SIGNED VINYL ALBUM RECORDS" + "      JAY CHOU ACTION FIGURE");
-    static String c3 = String.format("\n   RM 150 per T-SHIRT " + "            RM 300 per RECORD" + "                            RM800 per FIGURE");
+    public static String c1 = String.format("\n1) BLACKPINK CONCERT" + "           2) TAYLOR SWIFT CONCERT" + "                      3) JAY CHOU CONCERT" + "          4) PURCHASE COMPLETE/EXIT");
+    public static String c2 = String.format("\n   BLACKPINK SIGNED T-SHIRTS" + "      TAYLOR SWIFT SIGNED VINYL ALBUM RECORDS" + "      JAY CHOU ACTION FIGURE");
+    public static String c3 = String.format("\n   RM 150 per T-SHIRT " + "            RM 300 per RECORD" + "                            RM800 per FIGURE");
 
     public static void menu() {
         System.out.print(c1);
@@ -58,76 +61,41 @@ public class Merchandise2 {
 
     public static void done(double runningTotal) {
         ordering = false;
+        
+        System.out.println("total purchased : " + runningTotal);
         System.out.println("Please come again, thank you!");
     }
 
-        public static void Selection() {
+    
+    public static void Selection(int option){
+        
         int menuOption;
         int merchItem;
-        Scanner input = new Scanner(System.in);
-        do {
-            double runningTotal2 = 0;
-            menu();
-            menuOption = input.nextInt();
-            switch (menuOption) {
+        
+        switch (option) {
                 case 1:
                     merchItem = 1;
-                    runningTotal2 += itemPrice(merchItem);
-                    Stock.MerchStock_1(quantity2);
+                    
+                    itemPrice(merchItem);
+                    MerchStock_1(quantity2);
                     break;
                 case 2:
                     merchItem = 2;
-                    runningTotal2 += itemPrice(merchItem);
-                    Stock.MerchStock_2(quantity2);
+                    
+                    itemPrice(merchItem);
+                    MerchStock_2(quantity2);
                     break;
                 case 3:
                     merchItem = 3;
-                    runningTotal2 += itemPrice(merchItem);
-                    Stock.MerchStock_3(quantity2);
+                    
+                    itemPrice(merchItem);
+                    MerchStock_3(quantity2);
                     break;
                 case 4:
                     done(runningTotal);
                     break;
                 default:
                     System.out.println("Invalid option.");
-            }
-        } while (ordering);
-        System.out.println("Total amount: RM " + runningTotal);
     }
-    
-    
-    public static void main(String[] args) {
-
-        int menuOption;
-        int merchItem;
-        Scanner input = new Scanner(System.in);
-        do {
-            double runningTotal2 = 0;
-            menu();
-            menuOption = input.nextInt();
-            switch (menuOption) {
-                case 1:
-                    merchItem = 1;
-                    runningTotal2 += itemPrice(merchItem);
-                    Stock.MerchStock_1(quantity2);
-                    break;
-                case 2:
-                    merchItem = 2;
-                    runningTotal2 += itemPrice(merchItem);
-                    Stock.MerchStock_2(quantity2);
-                    break;
-                case 3:
-                    merchItem = 3;
-                    runningTotal2 += itemPrice(merchItem);
-                    Stock.MerchStock_3(quantity2);
-                    break;
-                case 4:
-                    done(runningTotal);
-                    break;
-                default:
-                    System.out.println("Invalid option.");
-            }
-        } while (ordering);
-        System.out.println("Total amount: RM " + runningTotal);
-    }
+}
 }
