e3a8fb2db04713a51348b8dc847eed081d684e7c
KennethMordon <79129049+KennethMordon@users.noreply.github.com>
2021-03-04 15:49:25 +0800

    Update Merchandise2.java

--- Merchandise2.java
+++ Merchandise2.java
@@ -12,16 +12,22 @@ public class Merchandise2 {
     private static double itemPrice;
     public static double runningTotal;
     static boolean ordering = true;
+    public static double quantity2;
+    static String c1 = String.format("\n1) BLACKPINK CONCERT" + "           2) TAYLOR SWIFT CONCERT" + "                      3) JAY CHOU CONCERT" + "          4) PURCHASE COMPLETE/EXIT");
+    static String c2 = String.format("\n   BLACKPINK SIGNED T-SHIRTS" + "      TAYLOR SWIFT SIGNED VINYL ALBUM RECORDS" + "      JAY CHOU ACTION FIGURE");
+    static String c3 = String.format("\n   RM 150 per T-SHIRT " + "            RM 300 per RECORD" + "                            RM800 per FIGURE");
 
-            
     public static void menu() {
-        System.out.println("Welcome \n1. BLACKPINK signed T-Shirts @ RM150 per pcs \n2. Taylor Swift signed Vinyl Records @ RM 300\n3. Jay Chou Action Figure @ RM 800 \n4. Purchase Complete");
+        System.out.print(c1);
+        System.out.print(c2);
+        System.out.println(c3);
     }
 
     public static double itemPrice(int merchItem) {
         if (merchItem == 1) {
             System.out.println("You've ordered Blackpink T shirt");
             itemPrice = 150.00;
+
         }
         if (merchItem == 2) {
             System.out.println("You've ordered Taylor Swift Vinyl Record ");
@@ -38,14 +44,14 @@ public class Merchandise2 {
     public static double quantity() {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter quantity");
-        double quantity = input.nextDouble();
-        subTotal(quantity, itemPrice);
-        return quantity;
+        Merchandise2.quantity2 = input.nextDouble();
+        subTotal(quantity2, itemPrice);
+        return quantity2;
     }
 
     public static double subTotal(double quantity, double itemPrice) {
         double subTotal = quantity * itemPrice;
-        System.out.println("Subtotal: " + subTotal);
+        System.out.println("Subtotal: RM " + subTotal);
         runningTotal += subTotal;
         return subTotal;
     }
@@ -68,14 +74,17 @@ public class Merchandise2 {
                 case 1:
                     merchItem = 1;
                     runningTotal2 += itemPrice(merchItem);
+                    Stock.MerchStock_1(quantity2);
                     break;
                 case 2:
                     merchItem = 2;
                     runningTotal2 += itemPrice(merchItem);
+                    Stock.MerchStock_2(quantity2);
                     break;
                 case 3:
                     merchItem = 3;
                     runningTotal2 += itemPrice(merchItem);
+                    Stock.MerchStock_3(quantity2);
                     break;
                 case 4:
                     done(runningTotal);
@@ -84,6 +93,6 @@ public class Merchandise2 {
                     System.out.println("Invalid option.");
             }
         } while (ordering);
-        System.out.println("Total amount: " + runningTotal);
+        System.out.println("Total amount: RM " + runningTotal);
     }
 }
