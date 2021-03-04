f123577184c72e5909dd701f311dbc35ae7f9027
LeeHanShen <77600251+LeeHanShen@users.noreply.github.com>
2021-03-04 16:26:35 +0800

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -11,6 +11,20 @@ import static assignment.Calc_payment.TicketPrice_CAT3;
 import static assignment.Calc_payment.TicketPrice_CAT4;
 import static assignment.Calc_payment.TicketPrice_CAT5;
 import static assignment.StockRemaining.BPstock_cat1;
+import static assignment.StockRemaining.BPstock_cat2;
+import static assignment.StockRemaining.BPstock_cat3;
+import static assignment.StockRemaining.BPstock_cat4;
+import static assignment.StockRemaining.BPstock_cat5;
+import static assignment.StockRemaining.Jaystock_cat1;
+import static assignment.StockRemaining.Jaystock_cat2;
+import static assignment.StockRemaining.Jaystock_cat3;
+import static assignment.StockRemaining.Jaystock_cat4;
+import static assignment.StockRemaining.Jaystock_cat5;
+import static assignment.StockRemaining.Taylorstock_cat1;
+import static assignment.StockRemaining.Taylorstock_cat2;
+import static assignment.StockRemaining.Taylorstock_cat3;
+import static assignment.StockRemaining.Taylorstock_cat4;
+import static assignment.StockRemaining.Taylorstock_cat5;
 import java.util.Scanner;
 
 /**
@@ -95,16 +109,16 @@ public class Interface {
             // prompt user to select ticket type
        // while(ticket > 5 && 1 >= ticket){    
        
-//             System.out.println("1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
-//                                  + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
+            System.out.println("1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
+                                 + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
             
-//             System.out.println("Please Select the type of ticket : ");
-//             int ticket = scan.nextInt();
-//             SelectTicket(ticket);
+            System.out.println("Please Select the type of ticket : ");
+            int ticket = scan.nextInt();
+            SelectTicket(ticket);
              
-//             //prompt user to select ticket quantity
-//             System.out.println("Please enter the quantity of ticket : ");
-//             int ticket1 = scan.nextInt();
+            //prompt user to select ticket quantity
+            System.out.println("Please enter the quantity of ticket : ");
+            int ticket1 = scan.nextInt();
             
          //prompt user whether wanna add tickets
             System.out.println("Do you want to add on ticket ? \n 1.YES \n 2. Continue to Payment");
@@ -119,7 +133,12 @@ public class Interface {
     
     
     
- 
+    
+    
+    
+    
+    
+    
         public static void selectConcert(int x) {
              Interface[] Interface1 ={new Interface(1,"Blackpink","2 March 2021 , 7pm"),
                                   new Interface(2,"Jay Chou","24 August 2021 , 7pm "),
@@ -129,22 +148,16 @@ public class Interface {
                 case 1:
                     System.out.println("You have selected ");
                     System.out.println(Interface1[x-1]);
-                    DisplayTicket();
-                    BPSelectTicket();
                     break;
                     
                 case 2:
                     System.out.println("You have selected ");
                     System.out.println(Interface1[x-1]);
-                    DisplayTicket();
-                    JaySelectTicket();
                     break;
                             
                 case 3:
                     System.out.println("You have selected ");
                     System.out.println(Interface1[x-1]);
-                    DisplayTicket();
-                    TaylorSelectTicket();    
                     break;
                     
                 default:
@@ -152,6 +165,152 @@ public class Interface {
             } 
        
     }
+        
+        public static void SelectTicket(int y){
+           Scanner scan  = new Scanner(System.in);
+           
+         switch (y){
+             
+                case 1:
+                  System.out.println("enter your quantity :");
+                  double quantity = scan.nextDouble();
+                  TicketPrice_CAT1(quantity);
+                  BPstock_cat1(quantity);
+                  break;
+                    //TicketPrice_CAT1(quantity);
+                    
+                case 2:
+                    System.out.println("enter your quantity :");
+                    double quantity1 = scan.nextDouble();
+                    TicketPrice_CAT2(quantity1);
+                    //TicketPrice_CAT2(quantity);
+                    break;
+                    
+                case 3:
+                    System.out.println("enter your quantity :");
+                    double quantity2 = scan.nextDouble();
+                    TicketPrice_CAT3(quantity2);
+                    
+                    break;
+                    
+                case 4:
+                    System.out.println("enter your quantity :");
+                    double quantity3 = scan.nextDouble();
+                    TicketPrice_CAT4(quantity3);
+                   break;
+                   
+                case 5:
+                    System.out.println("enter your quantity :");
+                    double quantity4 = scan.nextDouble();
+                    TicketPrice_CAT5(quantity4);
+                    break;
+                    
+                default :
+                    System.out.println("Type again");
+                    break;
+            }
+      
+        }
+        
+        public static void addon(int a){
+            Scanner scan = new Scanner(System.in);
+         for (int i = 0 ;i<2  ;i++)
+        switch (a) {
+            
+            case 1:
+                  System.out.println("1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
+                                 + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
+            
+            System.out.println("Please Select the type of ticket : ");
+            int ticket = scan.nextInt();
+            SelectTicket(ticket);
+             
+            //prompt user to select ticket quantity
+            System.out.println("Please enter the quantity of ticket : ");
+            int ticket1 = scan.nextInt();
+            
+                break;
+                
+            case 2:
+                
+                //proceed to payment
+                break;
+            default:
+                break;
+        }
+        }
+       
+        public static void displayConcert(){
+            System.out.println("\tConcert Available");
+        // ticket arrayS
+         Interface[] Interface1 ={new Interface(1,"Blackpink","2 March 2021 , 7pm"),
+                                  new Interface(2,"Jay Chou","24 August 2021 , 7pm "),
+                                  new Interface(3,"Taylor Swift","13 December 2021 , 7pm")};
+         //display array
+         int count = 1 ;
+        
+            for (Interface Interface11 : Interface1) {
+                System.out.println(Interface11);
+            }
+            
+            System.out.println("\nPlease Select Concert : ");
+          
+       
+        
+       
+       }
+        
+        public static void DisplayTicketBP(){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("\n1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
+                                 + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
+            
+            System.out.println("Please Select the type of ticket : ");
+            int ticket = scan.nextInt();
+            
+            BPSelectTicket(ticket);
+            
+             
+           
+            
+            
+            
+          }
+        
+        public static void DisplayTicketJay(){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("\n1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
+                                 + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
+            
+            System.out.println("Please Select the type of ticket : ");
+            int ticket = scan.nextInt();
+            
+            JaySelectTicket(ticket);
+            
+             
+           
+            
+            
+            
+          }
+        
+        public static void DisplayTicketTaylor(){
+            Scanner scan = new Scanner(System.in);
+            System.out.println("\n1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
+                                 + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
+            
+            System.out.println("Please Select the type of ticket : ");
+            int ticket = scan.nextInt();
+            
+            TaylorSelectTicket(ticket);
+            
+             
+           
+            
+            
+            
+          }
+        
         //blackpink ticket
         public static void BPSelectTicket(int y){
            Scanner scan  = new Scanner(System.in);
@@ -290,7 +449,7 @@ public class Interface {
                     System.out.println("enter your quantity :");
                     double quantity4 = scan.nextDouble();
                     TicketPrice_CAT5(quantity4);
-                    Taylorstock_cat5(quantity5);
+                    Taylorstock_cat5(quantity4);
                     break;
                     
                 default :
@@ -299,67 +458,4 @@ public class Interface {
             }
       
         }
-        
-        public static void addon(int a){
-            Scanner scan = new Scanner(System.in);
-         for (int i = 0 ;i<2  ;i++)
-        switch (a) {
-            
-            case 1:
-                  System.out.println("1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
-                                 + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
-            
-            System.out.println("Please Select the type of ticket : ");
-            int ticket = scan.nextInt();
-            SelectTicket(ticket);
-             
-            //prompt user to select ticket quantity
-            System.out.println("Please enter the quantity of ticket : ");
-            int ticket1 = scan.nextInt();
-            
-                break;
-                
-            case 2:
-                
-                //proceed to payment
-                break;
-            default:
-                break;
-        }
-        }
-       
-        public static void displayConcert(){
-            System.out.println("\tConcert Available");
-        // ticket arrayS
-         Interface[] Interface1 ={new Interface(1,"Blackpink","2 March 2021 , 7pm"),
-                                  new Interface(2,"Jay Chou","24 August 2021 , 7pm "),
-                                  new Interface(3,"Taylor Swift","13 December 2021 , 7pm")};
-         //display array
-         int count = 1 ;
-        
-            for (Interface Interface11 : Interface1) {
-                System.out.println(Interface11);
-            }
-            
-            System.out.println("\nPlease Select Concert : ");
-          
-       
-        
-       
-       }
-        
-        public static void DisplayTicket(){
-            Scanner scan = new Scanner(System.in);
-            System.out.println("\n1.CAT 1 [900] \n2.CAT 2 [800] \n3.CAT 3 [700] "
-                                 + "\n4.CAT 4 [600] \n5.CAT 5 [500]\n");
-            
-            System.out.println("Please Select the type of ticket : ");
-            int ticket = scan.nextInt();
-            SelectTicket(ticket);
-             
-           
-            
-            
-            
-          }
     }
