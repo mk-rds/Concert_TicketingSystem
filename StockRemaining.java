2d6f9e9d94f829dae061e568fb1cfa54b81f0deb
LeeHanShen <77600251+LeeHanShen@users.noreply.github.com>
2021-03-05 12:35:10 +0800

    Update StockRemaining.java

--- StockRemaining.java
+++ StockRemaining.java
@@ -5,6 +5,7 @@
  */
 package assignment;
 
+
 /**
  *
  * @author User
@@ -32,118 +33,120 @@ public class StockRemaining {
     public static double merch_2 = 100;
     public static double merch_3 = 100;
     
-    
+    public static boolean ticketOrder = true;
     
     
     public static void BPstock_cat1(double quantity){
-        bp_cat1=bp_cat1-quantity; 
-        double remaining = bp_cat1;
-        System.out.print("remaining :" + (int)remaining);
+        bp_cat1=bp_cat1-quantity;
+        double remaining;
+        remaining = bp_cat1;
+        
+      
     }
     
     public static void BPstock_cat2(double quantity){
         bp_cat2=bp_cat2-quantity;
         double remaining;
         remaining = bp_cat2;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
     
     public static void BPstock_cat3(double quantity){
         bp_cat3=bp_cat3-quantity;
         double remaining;
         remaining = bp_cat3;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
     
     public static void BPstock_cat4(double quantity){
         bp_cat4=bp_cat4-quantity;
         double remaining;
         remaining = bp_cat4;
-        System.out.print("remaining :" + (int)remaining);
+       
     }
     
     public static void BPstock_cat5(double quantity){
         bp_cat5=bp_cat5-quantity;
         double remaining;
         remaining = bp_cat5;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
     
     public static void Jaystock_cat1(double quantity){
         jay_cat1=jay_cat1-quantity;
         double remaining;
         remaining = jay_cat1;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
     
     public static void Jaystock_cat2(double quantity){
         jay_cat2=jay_cat2-quantity;
         double remaining;
         remaining = jay_cat2;
-        System.out.print("remaining :" + (int)remaining);
+       
     }
     
     public static void Jaystock_cat3(double quantity){
         jay_cat3=jay_cat3-quantity;
         double remaining;
         remaining = jay_cat3;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
     
     public static void Jaystock_cat4(double quantity){
         jay_cat4=jay_cat4-quantity;
         double remaining;
         remaining = jay_cat4;
-        System.out.print("remaining :" + (int)remaining);
+       
     }
     
     public static void Jaystock_cat5(double quantity){
         jay_cat5=jay_cat5-quantity;
         double remaining;
         remaining = jay_cat5;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
     
      public static void Taylorstock_cat1(double quantity){
         taylor_cat1=taylor_cat1-quantity;
         double remaining;
         remaining = taylor_cat1;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
      
      public static void Taylorstock_cat2(double quantity){
         taylor_cat2=taylor_cat2-quantity;
         double remaining;
         remaining = taylor_cat2;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
      
      public static void Taylorstock_cat3(double quantity){
         taylor_cat3=taylor_cat3-quantity;
         double remaining;
         remaining = taylor_cat3;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
      
      public static void Taylorstock_cat4(double quantity){
         taylor_cat4=taylor_cat4-quantity;
         double remaining;
         remaining = taylor_cat4;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
      
      public static void Taylorstock_cat5(double quantity){
         taylor_cat5=taylor_cat5-quantity;
         double remaining;
         remaining = taylor_cat5;
-        System.out.print("remaining :" + (int)remaining);
+        
     }
     
     public static void MerchStock_1(double quantity){
         merch_1 = merch_1 - quantity;
         double remaining;
         remaining = merch_1;
-        System.out.print("remaining : " + (int)remaining);
+        System.out.print("\nremaining stock :" + (int)remaining);
                
     }
     
@@ -151,7 +154,8 @@ public class StockRemaining {
         merch_2 = merch_2 - quantity;
         double remaining;
         remaining = merch_2;
-        System.out.print("remaining : " + (int)remaining);
+        System.out.print("\nremaining stock :" + (int)remaining);
+       
                
     }
     
@@ -159,7 +163,8 @@ public class StockRemaining {
         merch_3 = merch_3 - quantity;
         double remaining;
         remaining = merch_3;
-        System.out.print("remaining : " + (int)remaining);
+        System.out.print("\nremaining stock :" + (int)remaining);
+        
                
     }
 
